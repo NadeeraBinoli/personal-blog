24040330ea606ae206983928082720e1f72293d0
Nadeera_Binoli <95376457+NadeeraBinoli@users.noreply.github.com>
2022-08-07 00:17:38 +0530

    create menue-search  button
    create working search  button

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//  hamberger-menue button
 const navLinkUl = document.querySelector(".nav-link-ul");
 const hambergerMenue = document.querySelector(".hamberger-menue");
 const menueClose = document.querySelector(".menue-close");
@@ -13,5 +14,18 @@ menueClose.addEventListener('click',()=> {
   const dataVisible =  navLinkUl.getAttribute("data-visible");
  if (dataVisible==="true") {
   navLinkUl.setAttribute("data-visible", false);
+}
+}) ;
+
+// menue-search button
+const searchForm = document.querySelector(".search-form");
+const menueSearchButton = document.querySelector(".menue-search");
+
+menueSearchButton.addEventListener('click',()=> {
+const visibility =  searchForm.getAttribute("data-visibility");
+if (visibility === "false") {
+  searchForm.setAttribute("data-visibility", true);
+} else if (visibility==="true") {
+  searchForm.setAttribute("data-visibility", false);
 }
 }) ;
